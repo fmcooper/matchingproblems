6585c8ef36e1021611b271cf3dd50d6f29433e69
Frances Cooper <f.cooper.1@research.gla.ac.uk>
2020-10-03 12:58:58 +0100

    1st draft distribution

--- matchingproblems/generator/__init__.py
+++ matchingproblems/generator/__init__.py
@@ -1,4 +1,3 @@
 # __init__.py
 
 from .generator import Generator
-
--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
-from setuptools import setup
+from setuptools import find_packages, setup
 
 setup(
 	name='matchingproblems',
     version='0.1',
-    packages=['matchingproblems'],
+    packages=find_packages(),
     license='MIT',
     description='A matching problem generator and solver.',
     long_description=open('README.md').read(),
