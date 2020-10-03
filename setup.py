c341353479704d78b5461950a054c7bc26d2f117
Frances Cooper <f.cooper.1@research.gla.ac.uk>
2020-10-03 11:23:14 +0100

    license and setup files updated

--- setup.py
+++ setup.py
@@ -1,11 +1,14 @@
 from setuptools import setup
 
-setup(name='matcher',
-      version='0.1',
-      description='A matching allocator.',
-      url='http://github.com/fmcooper',   # to do
-      author='Frances Cooper',
-      author_email='fmcooper234@gmail.com',
-      license='MIT',
-      packages=['matcher'],
-      zip_safe=False)
+setup(
+	name='matchingproblems',
+    version='0.1',
+    packages=['matchingproblems'],
+    license='MIT',
+    description='A matching problem generator and solver.',
+    long_description=open('README.md').read(),
+    install_requires=['numpy', 'pulp'],
+    url='http://github.com/fmcooper/matchingproblems',
+    author='Frances Cooper',
+    author_email='fmcooper234@gmail.com'
+)
