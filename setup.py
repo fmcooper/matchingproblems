a6d0a716371cb047f03355a6f86436411fa2007f
David Maxwell <maxwelld90@gmail.com>
2020-10-03 13:41:04 +0100

    Updated requirements and setup files

--- setup.py
+++ setup.py
@@ -5,10 +5,17 @@ setup(
     version='0.1',
     packages=find_packages(),
     license='MIT',
+
     description='A matching problem generator and solver.',
     long_description=open('README.md').read(),
-    install_requires=['numpy', 'pulp'],
+    
     url='http://github.com/fmcooper/matchingproblems',
     author='Frances Cooper',
-    author_email='fmcooper234@gmail.com'
+    author_email='fmcooper234@gmail.com',
+
+    python_requires='>=3.6',
+    install_requires=[
+        'numpy==1.16.2',
+        'pulp==2.3'
+    ]
 )
