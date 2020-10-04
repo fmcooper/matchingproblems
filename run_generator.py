c2481dde414457ca28053795ebcfe4ea7d0a8a90
Frances Cooper <f.cooper.1@research.gla.ac.uk>
2020-10-04 08:29:59 +0100

    remove root example files

--- run_generator.py
+++
@@ -1,9 +0,0 @@
-from matchingproblems import generator
-import sys
-
-"""Simple script to generate matchings specified by the given arguments."""
-if __name__ == "__main__":
-    generator = generator.Generator(sys.argv[1:])
-
-
-
